0d6da49e6fb27cf57ba865cdeea271cad689b762
mohamedzouine827 <mohamedzouine82o@gmail.com>
2024-06-24 13:36:56 -0700

    8

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -4,6 +4,7 @@ export default function taskBlock(trueOrFalse) {
     var task2 = true;
 
     if (trueOrFalse) {
+        /* eslint-disable no-unused-vars */
         const task = true;
         const task2 = false;
     }
