b4411a9bcb34d75e01d1cee2751c67253f620ad9
mohamedzouine827 <mohamedzouine82o@gmail.com>
2024-06-27 13:41:52 -0700

    8

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,19 @@
-#!/usr/bin/node
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  let firstName;
+  let lastName;
+  let body;
+
+  const p = uploadPhoto();
+  const u = createUser();
+
+  return Promise.all([p, u]).then((data) => {
+    body = data[0].body;
+    firstName = data[1].firstName;
+    lastName = data[1].lastName;
+    console.log(`${body} ${firstName} ${lastName}`);
+  }).catch(() => {
+    console.log('Signup system offline');
+  });
+}
--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+export default function guardrail(mathFunction) {
+  const queue = [];
+  let res;
+  try {
+    res = mathFunction();
+  } catch (error) {
+    res = `Error: ${error.message}`;
+  }
+  queue.push(res);
+  queue.push('Guardrail was processed');
+
+  return queue;
+}
